eaaf42cd150e3ed06ef8fc02ac6e1eb209c8f76e
elo240 <sebastian.kryszczuk47+GIT@gmail.com>
2020-06-16 10:35:18 +0200

    Updates

---
+++ precache-manifest.801fab0ff757e0848fdf74f8e44dbf07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78df62fef5033a5c263bf5748cc3036d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c0778439cc1ccb16641c",
+    "url": "./static/css/main.64df07de.chunk.css"
+  },
+  {
+    "revision": "141b57dc394acaf0f44a",
+    "url": "./static/js/2.3a802bda.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "./static/js/2.3a802bda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0778439cc1ccb16641c",
+    "url": "./static/js/main.ff2daae1.chunk.js"
+  },
+  {
+    "revision": "cd245304771a154588f7",
+    "url": "./static/js/runtime-main.06af967e.js"
+  }
+]);
